ff70290904126adaf853263aceb545c86e37f824
Alisson <alissonhmb@gmail.com>
2022-12-29 16:37:05 -0300

    Add comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+  
+  def create
+    @comment = @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+  private
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def comment_params
+      params[:comment].permit(:name, :body)
+    end
+end
